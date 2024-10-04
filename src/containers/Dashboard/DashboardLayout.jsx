5061080814bc34dd834f5e8d09b4d41c99c8ac8c
Amir Tadrisi <amirtds@gmail.com>
2024-10-04 15:53:19 -0500

    feat: Add redeem enrollment code widget

--- src/containers/Dashboard/DashboardLayout.jsx
+++ src/containers/Dashboard/DashboardLayout.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Container, Col, Row } from '@openedx/paragon';
 
 import WidgetSidebar from '../WidgetContainers/WidgetSidebar';
-
+import RedeemEntitlementCode from './RedeemEntitlementCode';
 import hooks from './hooks';
 
 export const columnConfig = {
@@ -43,6 +43,7 @@ export const DashboardLayout = ({ children }) => {
         <Col {...columnConfig.sidebar} className="sidebar-column">
           {!isCollapsed && (<h2 className="course-list-title">&nbsp;</h2>)}
           <WidgetSidebar />
+          <RedeemEntitlementCode />
         </Col>
       </Row>
     </Container>
