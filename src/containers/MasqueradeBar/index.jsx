80f80474838679e6802d74491d31342f4630032d
Amir Tadrisi <amirtds@gmail.com>
2024-10-04 18:18:47 -0500

    chore: adjust the masquerade bar css

--- src/containers/MasqueradeBar/index.jsx
+++ src/containers/MasqueradeBar/index.jsx
@@ -37,54 +37,56 @@ export const MasqueradeBar = () => {
 
   return (
     <div className="w-100 shadow-sm px-2 d-flex justify-content-center">
-      <Form className="masquerade-bar d-flex align-items-center">
-        {isMasquerading ? (
-          <>
-            <FormLabel inline className="masquerade-form-label mb-0 mr-2">
-              <Icon src={PersonSearch} className="mr-2" />
-              {formatMessage(messages.ViewingAs)}
-            </FormLabel>
-            <Chip
-              className="masquerade-chip"
-              iconAfter={Close}
-              onClick={handleClearMasquerade}
-            >
-              {masqueradeInput}
-            </Chip>
-          </>
-        ) : (
-          <>
-            <FormLabel inline id="masquerade-form-label" className="masquerade-form-label mb-0 mr-2">
-              <Icon src={PersonSearch} className="mr-2" />
-              {formatMessage(messages.ViewAs)}
-            </FormLabel>
-            <FormGroup isInvalid={isMasqueradingFailed} className="masquerade-form-input mb-0 mr-2">
-              <FormControl
-                value={masqueradeInput}
-                onChange={handleMasqueradeInputChange}
-                floatingLabel={formatMessage(messages.StudentNameInput)}
-                aria-labelledby="masquerade-form-label"
+      <div className="masquerade-container w-100 max-width-container">
+        <Form className="masquerade-bar d-flex align-items-center justify-content-center">
+          {isMasquerading ? (
+            <>
+              <FormLabel inline className="masquerade-form-label mb-0 mr-2">
+                <Icon src={PersonSearch} className="mr-2" />
+                {formatMessage(messages.ViewingAs)}
+              </FormLabel>
+              <Chip
+                className="masquerade-chip"
+                iconAfter={Close}
+                onClick={handleClearMasquerade}
+              >
+                {masqueradeInput}
+              </Chip>
+            </>
+          ) : (
+            <>
+              <FormLabel inline id="masquerade-form-label" className="masquerade-form-label mb-0 mr-2">
+                <Icon src={PersonSearch} className="mr-2" />
+                {formatMessage(messages.ViewAs)}
+              </FormLabel>
+              <FormGroup isInvalid={isMasqueradingFailed} className="masquerade-form-input mb-0 mr-2 flex-grow-1">
+                <FormControl
+                  value={masqueradeInput}
+                  onChange={handleMasqueradeInputChange}
+                  floatingLabel={formatMessage(messages.StudentNameInput)}
+                  aria-labelledby="masquerade-form-label"
+                />
+                {isMasqueradingFailed && (
+                  <FormControlFeedback type="invalid" hasIcon={false}>
+                    {formatMessage(masqueradeErrorMessage)}
+                  </FormControlFeedback>
+                )}
+              </FormGroup>
+              <StatefulButton
+                disabled={!masqueradeInput.length}
+                variant="brand"
+                onClick={handleMasqueradeSubmit(masqueradeInput)}
+                labels={{
+                  default: formatMessage(messages.SubmitButton),
+                }}
+                className="ml-2"
+                state={isMasqueradingPending ? 'pending' : 'default'}
+                type="submit"
               />
-              {isMasqueradingFailed && (
-                <FormControlFeedback type="invalid" hasIcon={false}>
-                  {formatMessage(masqueradeErrorMessage)}
-                </FormControlFeedback>
-              )}
-            </FormGroup>
-            <StatefulButton
-              disabled={!masqueradeInput.length}
-              variant="brand"
-              onClick={handleMasqueradeSubmit(masqueradeInput)}
-              labels={{
-                default: formatMessage(messages.SubmitButton),
-              }}
-              className="ml-2"
-              state={isMasqueradingPending ? 'pending' : 'default'}
-              type="submit"
-            />
-          </>
-        )}
-      </Form>
+            </>
+          )}
+        </Form>
+      </div>
     </div>
   );
 };
