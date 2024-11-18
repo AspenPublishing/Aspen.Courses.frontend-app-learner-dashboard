7a4e3c87bbf8b7ea7847dde30729478961044563
Amir Tadrisi <amirtds@gmail.com>
2024-11-18 08:20:54 -0500

    chore: justify filter elements to end

--- src/containers/CoursesPanel/index.jsx
+++ src/containers/CoursesPanel/index.jsx
@@ -32,7 +32,7 @@ export const CoursesPanel = () => {
     <div className="course-list-container">
       <div className="course-list-heading-container py-4">
         <h2 className="course-list-title">{formatMessage(messages.myCourses)}</h2>
-        <div className="course-filter-controls-container flex">
+        <div className="course-filter-controls-container flex justify-content-end">
           <Button variant="outline-primary" className="mr-2">ENROLLMENT CODE</Button>
           <CourseFilterControls {...courseListData.filterOptions} />
         </div>
